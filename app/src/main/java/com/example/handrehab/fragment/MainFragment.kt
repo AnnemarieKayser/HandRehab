e77a7cc2875c3b62958ccab02f9bb2cc0568ffa1
AnnemarieKayser <annemarie.kayser@stud.hshl.de>
2023-11-08 08:37:18 +0100

    menu logout

--- app/src/main/java/com/example/handrehab/fragment/MainFragment.kt
+++ app/src/main/java/com/example/handrehab/fragment/MainFragment.kt
@@ -2,8 +2,12 @@ package com.example.handrehab.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
@@ -40,18 +44,17 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setHasOptionsMenu(true)
+
+
         binding.buttonGoToData.setOnClickListener {
             findNavController().navigate(R.id.action_MainFragment_to_dataFragment)
         }
 
-        binding.buttonGoToSettings.setOnClickListener {
-            findNavController().navigate(R.id.action_MainFragment_to_settingFragment)
-        }
 
         binding.buttonToExerciseList.setOnClickListener {
             findNavController().navigate(R.id.action_MainFragment_to_exerciseListFragment)
         }
-
 
 
         binding.buttonLogOut.setOnClickListener {
@@ -60,6 +63,32 @@ class MainFragment : Fragment() {
             activity?.startActivity(intent)
         }
     }
+
+    // === onCreateOptionsMenu === //
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.menu_main_2, menu)
+
+    }
+
+    // === onOptionsItemSelected === //
+    // Hier werden Klicks auf Elemente der Aktionsleiste behandelt
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.menu_item_log_out-> {
+                mFirebaseAuth.signOut()
+                val intent = Intent(activity, LoginInActivity::class.java)
+                activity?.startActivity(intent)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
